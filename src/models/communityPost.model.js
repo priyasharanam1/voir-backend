34a13a63efcff511a79fa74956a84faecff1776c
priyasharanam1 <p1910413219@gmail.com>
2024-08-28 18:57:11 +0530

    added likes, comments, playlist, community post models

---
+++ src/models/communityPost.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const communityPostSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+  },
+  { timestamps: true }
+);
+
+export const CommunityPost = mongoose.model("CommunityPost", communityPostSchema);
