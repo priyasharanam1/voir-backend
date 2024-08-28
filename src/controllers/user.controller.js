34dd36cdfa580c0f36fe1a3664df1549dd2c3096
priyasharanam1 <p1910413219@gmail.com>
2024-08-28 18:34:11 +0530

    history controller fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import mongoose from "mongoose";
 import {
   uploadOnCloudinary,
   deleteFromCloudinary,
