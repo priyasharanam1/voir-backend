1627a69ef79ad5453b18065056cf8fbdb3eac5cd
priyasharanam1 <p1910413219@gmail.com>
2024-08-28 18:14:08 +0530

    logout controller fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,7 +172,7 @@ const loginUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
   // Remove the refresh token from the user's record
   await User.findByIdAndUpdate(req.user._id, {
-    $set: { refreshToken: undefined },
+    $unset: { refreshToken: 1 },
   });
 
   const cookieOptions = {
